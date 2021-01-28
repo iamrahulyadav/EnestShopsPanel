6e4e4a01346d34e51fc5a0ae7a238fa00e4e3c95
Subhankar Pal <subho.playboy10@gmail.com>
2021-01-28 21:45:41 +0530

    fixing links test 2

--- productlist.php
+++ productlist.php
@@ -57,7 +57,7 @@ require 'include/header.php';
                                                         <tr>
                                                             <td><?php echo $i; ?></td>
                                                             <td><?php echo $rkl['pname']; ?></td>
-                                                            <td><img src="<?php echo dirname(dirname(__FILE__)) . "/manage/" . $rkl['pimg']; ?>" width="100" height="100" /></td>
+                                                            <td><img src="https://manage.e-nest.in/<?php echo $rkl['pimg']; ?>" width="100" height="100" /></td>
                                                             <td><?php $sb = explode(',', $rkl['prel']);
 
 
@@ -65,7 +65,7 @@ require 'include/header.php';
                                                                     if ($bb == '') {
                                                                     } else {
                                                                 ?>
-                                                                        <img src="<?php echo dirname(dirname(__FILE__)) . "/manage/" . $bb; ?>" width="100" height="100" />
+                                                                        <img src="https://manage.e-nest.in/<?php echo $bb; ?>" width="100" height="100" />
                                                                 <?php
                                                                     }
                                                                 }
